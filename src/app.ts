dc563fbb3489c204d6a4642f6af1ae55591474ff
Mantas Lastakauskas <mantaslastakauskas@wixs-MBP.zyxel.com>
2022-03-09 16:33:11 +0200

    Dotenv and non relative imports

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
 import express, { Application, Request, Response } from 'express';
+import { PORT } from '@/config/index';
+import db from './db';
+
+db.authenticate()
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    // eslint-disable-next-line no-console
+    console.error('Unable to connect to the database:', err);
+  });
 
 const app: Application = express();
-const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello world');
 });
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server started on port ${PORT}`);
 });
