11d5de72b2b90aa5321826a46f4dd73c7e811146
Mantas Lastakauskas <mantaslastakauskas@wixs-MBP.zyxel.com>
2022-03-09 21:22:17 +0200

    old db remove

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,5 @@
 import express, { Application, Request, Response } from 'express';
 import { PORT } from '@/config';
-import db from '@/db';
-
-db.authenticate()
-  .then(() => {
-    // eslint-disable-next-line no-console
-    console.log('Connection has been established successfully.');
-  })
-  .catch((err: Error) => {
-    // eslint-disable-next-line no-console
-    console.error('Unable to connect to the database:', err);
-  });
 
 const app: Application = express();
 
